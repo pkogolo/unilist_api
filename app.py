630d501f2ed547bdf1292e9adeb89725a23c65bd
pkogolo <onyivana07@gmail.com>
2023-09-03 03:37:20 +0100

    looking into the output format

--- app.py
+++ app.py
@@ -16,39 +16,39 @@ app = Flask(__name__)
 def unilist():
     uni_url = "http://universities.hipolabs.com/search?country=Nigeria"
 
-    #Also define a file path to write the output
-    #path = "c:\\Users\\kogos\\OneDrive\\Desktop\\Python\\NGN_Universities.txt"
+        #Also define a file path to write the output
+        #path = "c:\\Users\\kogos\\OneDrive\\Desktop\\Python\\NGN_Universities.txt"
 
     data = requests.get(uni_url)
     uni_data = data.content
 
-    #use jason.loads() to parse the JSON and store in uni_list
+        #use jason.loads() to parse the JSON and store in uni_list
     uni_list = json.loads(uni_data)
 
     uni_accumulator = len(uni_list) #populate the counter
     print("The Number Of Universities Published in Nigeria is:", uni_accumulator)
 
-    # Create an empty array to store university names
+        # Create an empty array to store university names
     uni_names = []
 
-    #This loops generate serial numbers and append tem to the
-    #corresponding univeristy names on the list
+        #This loops generate serial numbers and append tem to the
+        #corresponding univeristy names on the list
     for uni_num, uni in enumerate(uni_list, start=1):
-        names = uni["name"] #the variable "name" is used in each uni dictionary
-        numbered_names = f"{uni_num}.{names}"
-        uni_names.append(numbered_names)
-        for serial_names in uni_names:
-            print(serial_names)
+            names = uni["name"] #the variable "name" is used in each uni dictionary
+            numbered_names = f"{uni_num}.{names}"
+            uni_names.append(numbered_names)
+            for serial_names in uni_names:
+                print(serial_names)
     return json.dumps(uni_names)
 
-    # with open(path, "w") as file: #open file outside the loop prevents overwrite  
-    #     for serial_names in uni_names:
-    #         print(serial_names)
-    #         file.write(serial_names + "\n")
+        # with open(path, "w") as file: #open file outside the loop prevents overwrite  
+        #     for serial_names in uni_names:
+        #         print(serial_names)
+        #         file.write(serial_names + "\n")
 
 if __name__ == "__main__":
-	#app.run()
-    app.run(host="0.0.0", port=7000)
+# 	#app.run()
+   app.run(host="0.0.0", port=7000)
 
 
 
