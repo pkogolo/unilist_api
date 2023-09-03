8fca5d567788cf8b1f2098c8d59fcf341de23243
pkogolo <onyivana07@gmail.com>
2023-09-03 02:32:57 +0100

    modifying the whole unilist api app and rtecpnfiguring the repo

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+#This program grabs data from an api website that publishes
+#the list of all universities in a every country then 
+#accummulate the number of universities in a country e.g Nigeria
+#then publishes the names of the universities in a seperate array
+#used the len() to get the cummulative number of published university
+#used the enumerate() to iterate over tyhe university list tracking the 
+#their index(numbers) and the element(university name)
+#Then print the universities serially in alphabetic order
 from flask import Flask
 import requests
 import json
@@ -5,11 +13,12 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
-def uni_lister():
+def unilist():
     uni_url = "http://universities.hipolabs.com/search?country=Nigeria"
 
     #Also define a file path to write the output
     #path = "c:\\Users\\kogos\\OneDrive\\Desktop\\Python\\NGN_Universities.txt"
+
     data = requests.get(uni_url)
     uni_data = data.content
 
@@ -28,10 +37,16 @@ def uni_lister():
         names = uni["name"] #the variable "name" is used in each uni dictionary
         numbered_names = f"{uni_num}.{names}"
         uni_names.append(numbered_names)
-
+        for serial_names in uni_names:
+            print(serial_names)
     return json.dumps(uni_names)
 
+    # with open(path, "w") as file: #open file outside the loop prevents overwrite  
+    #     for serial_names in uni_names:
+    #         print(serial_names)
+    #         file.write(serial_names + "\n")
 
 if __name__ == "__main__":
-	app.run()
+	#app.run()
+    app.run(host='0.0.0', port=7000)
 
---
+++ dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.9-alpine
+WORKDIR /app
+COPY . /app
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+EXPOSE 7000
+CMD python ./app.py
